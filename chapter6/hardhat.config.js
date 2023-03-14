7ffecbea7621159ba7e741b59d28d54772d1c4b6
Tushar Gupta <kshitizgupta108@gmail.com>
2023-03-14 20:19:13 +0530

    deployed this on test net

--- chapter6/hardhat.config.js
+++ chapter6/hardhat.config.js
@@ -1,6 +1,19 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config()
 
 /** @type import('hardhat/config').HardhatUserConfig */
+
+const RINKEBY_RPC_URL = process.env.RINKEBY_RPC_URL
+const PRIVATE_KEY = process.env.PRIVATE_KEY
+
 module.exports = {
+  defaultNetwork:"hardhat",
+  networks:{
+    rinkeby :{
+      url : RINKEBY_RPC_URL,
+      accounts : [PRIVATE_KEY],
+      chainID : 4,
+    }
+  },
   solidity: "0.8.18",
 };
