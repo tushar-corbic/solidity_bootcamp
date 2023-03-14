df9a2da405c358085a0edce6baf2e5cf90fc9bfc
Tushar Gupta <kshitizgupta108@gmail.com>
2023-03-14 21:13:05 +0530

    verified my contract and deployed

--- chapter6/hardhat.config.js
+++ chapter6/hardhat.config.js
@@ -1,11 +1,11 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config()
-
+require("@nomiclabs/hardhat-etherscan")
 /** @type import('hardhat/config').HardhatUserConfig */
 
 const RINKEBY_RPC_URL = process.env.RINKEBY_RPC_URL
 const PRIVATE_KEY = process.env.PRIVATE_KEY
-
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
 module.exports = {
   defaultNetwork:"hardhat",
   networks:{
@@ -14,6 +14,9 @@ module.exports = {
       accounts : [PRIVATE_KEY],
       chainID : 4,
     }
+  },
+  etherscan :{
+    apiKey : ETHERSCAN_API_KEY,
   },
   solidity: "0.8.18",
 };
--- chapter6/scripts/deploy.js
+++ chapter6/scripts/deploy.js
@@ -1,5 +1,5 @@
 // imports
-const { ethers } = require("hardhat")
+const { ethers, run , network } = require("hardhat")
 
 
 // main function
@@ -9,10 +9,39 @@ async function main(){
   const simpleStorage = await SimpleStorageFactory.deploy();
   await simpleStorage.deployed();
 
-  console.log(`Deployed contract at the address ${simpleStorage.address}`)
+  console.log(`Deployed contract at the address ${simpleStorage.address}`);
+  console.log(network.config);
+  if(network.config.chainId===4 && process.env.ETHERSCAN_API_KEY){
+    await simpleStorage.deployTransaction.wait(6);
+    await verify(simpleStorage.address, []);
+  }
+
+  const currentValue = await simpleStorage.retrieve();
+  console.log(`The current value is :${currentValue}`);
+
+  const transactionResponse = await simpleStorage.store(7);
+  await transactionResponse.wait(1);
+  const updatedValue = await simpleStorage.retreive();
+  console.log(`The updated value is ${updatedValue}`);
 }
 
 
+async function verify(contractAddress, args){
+  console.log("Verifying contract...")
+
+  try{
+    await run("verify:verify", {
+      address: contractAddress,
+      constructorArguments: args,
+    })
+  }catch(error){
+    if (error.message.toLowerCase().includes("already Verified")){
+      console.log("Already Verified");
+    }else{
+      console.log(error);
+    }
+  }
+}
 
 // call the main funtion
 main().then(()=> process.exit(0)).catch((error)=>{
